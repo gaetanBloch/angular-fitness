bed6572f48eced38b226b4ff6d5546c2c567f160
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-27 09:35:48 +0200

    Add an Angular Material Button

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+
+
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [MatButtonModule],
+  exports: [MatButtonModule]
 })
 export class MaterialModule {
 
