2d7c2be448e057707e0c0bfb6787ef089267d2d8
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-27 20:13:16 +0200

    Split the AppComponent into sidenav-list and header Components

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.css']
+})
+export class SidenavListComponent implements OnInit {
+  @Output() closeSideNav = new EventEmitter<void>();
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onClose(): void {
+    this.closeSideNav.emit();
+  }
+}
