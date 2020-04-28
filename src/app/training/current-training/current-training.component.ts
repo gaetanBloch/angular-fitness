46b0fe9c61cb8110fffacefb19b8aec9529c6178
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-28 12:29:44 +0200

    Added Exit and Continue actions in the dialog

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training/stop-training.component';
 
@@ -8,6 +8,7 @@ import { StopTrainingComponent } from './stop-training/stop-training.component';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter<void>();
   progress = 0;
   timer: number;
 
@@ -15,21 +16,33 @@ export class CurrentTrainingComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.startOrResumeTimer();
+  }
+
+  onStop(): void {
+    clearInterval(this.timer);
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }
+    });
+  }
+
+  private startOrResumeTimer(): void {
     this.timer = setInterval(() => {
       this.progress += 5;
       if (this.progress >= 100) {
         clearInterval(this.timer);
       }
     }, 1000);
-  }
-
-  onStop(): void {
-    clearInterval(this.timer);
-    this.dialog.open(StopTrainingComponent, {
-      data: {
-        progress: this.progress
-      }
-    });
   }
 }
 
