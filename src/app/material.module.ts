d0ed96382ce50fb4c935b6e42e95afe1f83a864c
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-28 11:12:44 +0200

    Display a spinner and the progression in the current training Component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -13,6 +13,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   imports: [
@@ -28,7 +29,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatListModule,
     MatTabsModule,
     MatCardModule,
-    MatSelectModule
+    MatSelectModule,
+    MatProgressSpinnerModule
   ],
   exports: [
     MatButtonModule,
@@ -43,7 +45,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatListModule,
     MatTabsModule,
     MatCardModule,
-    MatSelectModule
+    MatSelectModule,
+    MatProgressSpinnerModule
   ]
 })
 export class MaterialModule {
--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  progress = 0;
+  interval: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.interval = setInterval(() => {
+      if (this.progress === 100) {
+        clearInterval(this.interval);
+      } else {
+        this.progress += 5;
+      }
+    }, 1000);
+  }
 }
