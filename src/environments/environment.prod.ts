6e98388ad04e7c40cd93b35d003a5d7cfc7a54af
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-29 09:22:08 +0200

    Fixed missing environment variables for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,8 @@ export const environment = {
     databaseURL: 'https://angular-fitness-f641e.firebaseio.com',
     projectId: 'angular-fitness-f641e',
     storageBucket: 'angular-fitness-f641e.appspot.com',
-    messagingSenderId: '767102177727'
+    messagingSenderId: '767102177727',
+    appId: '1:767102177727:web:f1d84a5556f9f14f21910a',
+    measurementId: 'G-24TQBZB483'
   }
 };
