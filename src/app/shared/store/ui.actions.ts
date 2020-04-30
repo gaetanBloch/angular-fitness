d773e19df763879e58d4eb4fa6e2cf57da72049b
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-30 10:28:33 +0200

    Created New UI Actions

--- src/app/shared/store/ui.actions.ts
+++ src/app/shared/store/ui.actions.ts
@@ -1,14 +1,9 @@
-import { Action } from '@ngrx/store';
+import { createAction } from '@ngrx/store';
 
-export const START_LOADING = '[UI] START LOADING';
-export const STOP_LOADING = '[UI] STOP LOADING';
+export const startLoading = createAction(
+  '[UI] START LOADING'
+);
 
-export class StartLoading implements Action {
-  readonly type = START_LOADING;
-}
-
-export class StopLoading implements Action {
-  readonly type = STOP_LOADING;
-}
-
-export type UiActions = StartLoading | StopLoading;
+export const stopLoading = createAction(
+  '[UI] START LOADING'
+);
