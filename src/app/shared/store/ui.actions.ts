0df90c94ef27ff74c77f9b97c39917f531bf221f
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-30 09:55:12 +0200

    Added Start and Stop Loading Actions

---
+++ src/app/shared/store/ui.actions.ts
@@ -0,0 +1,12 @@
+import { Action } from '@ngrx/store';
+
+export const START_LOADING = '[UI] START LOADING';
+export const STOP_LOADING = '[UI] STOP LOADING';
+
+export class StartLoading implements Action {
+  readonly type = START_LOADING;
+}
+
+export class StopLoading implements Action {
+  readonly type = STOP_LOADING;
+}
